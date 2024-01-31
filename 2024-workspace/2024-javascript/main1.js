397209eda3d541bcd94b93f1ffadf6563cec086c
Nikalikis <nikalikis@gmail.com>
2024-01-31 09:48:30 +0200

    chaosas

---
+++ 2024-workspace/2024-javascript/main1.js
@@ -0,0 +1,13 @@
+let vardas = 'Jonas';
+
+console.log(vardas + 'Pavardenis');
+
+console.log('Hello');
+
+let miestas = 'vilnius';
+ let adresas = 'Vaizganto g'
+ let telefonas = '+37063633630'
+ 
+//  console.log ('Gauti duomenys ' + miestas + adresas + telefonas)
+ 
+ console.log(`gauti duomenys: ${miestas} ${adresas} ${telefonas} `)
---
+++ 2024-workspace/2024-javascript/test.js
@@ -0,0 +1,6 @@
+console.log("Simonas Kaminskas 1993 2024")
+let vardas = Simonas;
+let pavarde = Kaminskas;
+let Gimimo = 1993;
+let data = 2024;
+ let amzius = data - Gimimo;
